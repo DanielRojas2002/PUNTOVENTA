85f251e8f3c550f5313ade5acef4f42f45048d80
danym <danymessi1000@gmail.com>
2023-02-10 16:20:48 -0600

    pantallas proveedor Damian

---
+++ MENU/PROVEEDOR/modificar_proveedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PUNTOVENTA.MENU.PROVEEDOR
+{
+    public partial class modificar_proveedor : Form
+    {
+        public modificar_proveedor()
+        {
+            InitializeComponent();
+        }
+    }
+}
