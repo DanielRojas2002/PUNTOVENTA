7e408239ce9c57b78fd57d1c1a2354c70ab288c8
danym <danymessi1000@gmail.com>
2023-02-10 16:46:24 -0600

    eliminar proveedor

--- MENU/PROVEEDOR/eliminar_proveedor.Designer.cs
+++ MENU/PROVEEDOR/eliminar_proveedor.Designer.cs
@@ -28,12 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(eliminar_proveedor));
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_regresar = new System.Windows.Forms.PictureBox();
+            this.bx_usuario = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btn_eliminar_usuario = new System.Windows.Forms.Button();
+            this.lbl_perfil = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.Label();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_regresar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.Location = new System.Drawing.Point(416, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 28);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Usuario";
+            // 
+            // btn_regresar
+            // 
+            this.btn_regresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_regresar.Image = ((System.Drawing.Image)(resources.GetObject("btn_regresar.Image")));
+            this.btn_regresar.Location = new System.Drawing.Point(153, 71);
+            this.btn_regresar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_regresar.Name = "btn_regresar";
+            this.btn_regresar.Size = new System.Drawing.Size(42, 39);
+            this.btn_regresar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_regresar.TabIndex = 39;
+            this.btn_regresar.TabStop = false;
+            // 
+            // bx_usuario
+            // 
+            this.bx_usuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(25)))), ((int)(((byte)(74)))));
+            this.bx_usuario.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.bx_usuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bx_usuario.ForeColor = System.Drawing.Color.White;
+            this.bx_usuario.FormattingEnabled = true;
+            this.bx_usuario.Location = new System.Drawing.Point(454, 237);
+            this.bx_usuario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bx_usuario.Name = "bx_usuario";
+            this.bx_usuario.Size = new System.Drawing.Size(205, 28);
+            this.bx_usuario.TabIndex = 42;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(153, 167);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 259);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 37;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(430, 352);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(70, 53);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 40;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btn_eliminar_usuario
+            // 
+            this.btn_eliminar_usuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(25)))), ((int)(((byte)(74)))));
+            this.btn_eliminar_usuario.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_eliminar_usuario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_eliminar_usuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_eliminar_usuario.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_eliminar_usuario.ForeColor = System.Drawing.Color.White;
+            this.btn_eliminar_usuario.Location = new System.Drawing.Point(507, 352);
+            this.btn_eliminar_usuario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_eliminar_usuario.Name = "btn_eliminar_usuario";
+            this.btn_eliminar_usuario.Size = new System.Drawing.Size(249, 53);
+            this.btn_eliminar_usuario.TabIndex = 38;
+            this.btn_eliminar_usuario.Text = "ELIMINAR USUARIO";
+            this.btn_eliminar_usuario.UseVisualStyleBackColor = false;
+            // 
+            // lbl_perfil
+            // 
+            this.lbl_perfil.AutoSize = true;
+            this.lbl_perfil.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_perfil.ForeColor = System.Drawing.Color.White;
+            this.lbl_perfil.Location = new System.Drawing.Point(299, 89);
+            this.lbl_perfil.Name = "lbl_perfil";
+            this.lbl_perfil.Size = new System.Drawing.Size(73, 23);
+            this.lbl_perfil.TabIndex = 45;
+            this.lbl_perfil.Text = "perfil:";
+            this.lbl_perfil.Visible = false;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.AutoSize = true;
+            this.txt_usuario.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.txt_usuario.ForeColor = System.Drawing.Color.White;
+            this.txt_usuario.Location = new System.Drawing.Point(422, 84);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(95, 23);
+            this.txt_usuario.TabIndex = 44;
+            this.txt_usuario.Text = "usuario:";
+            this.txt_usuario.Visible = false;
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_id.ForeColor = System.Drawing.Color.White;
+            this.lbl_id.Location = new System.Drawing.Point(230, 84);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(30, 23);
+            this.lbl_id.TabIndex = 43;
+            this.lbl_id.Text = "id";
+            this.lbl_id.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(202, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 46);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "ELIMINAR USUARIO";
+            // 
+            // eliminar_proveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_regresar);
+            this.Controls.Add(this.bx_usuario);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btn_eliminar_usuario);
+            this.Controls.Add(this.lbl_perfil);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.lbl_id);
+            this.Controls.Add(this.label1);
+            this.Name = "eliminar_proveedor";
             this.Text = "eliminar_proveedor";
+            ((System.ComponentModel.ISupportInitialize)(this.btn_regresar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label2;
+        private PictureBox btn_regresar;
+        private ComboBox bx_usuario;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private Button btn_eliminar_usuario;
+        public Label lbl_perfil;
+        public Label txt_usuario;
+        public Label lbl_id;
+        private Label label1;
     }
 }
