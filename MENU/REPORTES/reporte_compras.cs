1ccdaa12efe2e02c7a517f56a3377d58f5a1ccaa
danym <danymessi1000@gmail.com>
2023-03-11 17:10:15 -0600

    FINALIZACION REPORTE COMPRAS

--- MENU/REPORTES/reporte_compras.cs
+++ MENU/REPORTES/reporte_compras.cs
@@ -1,4 +1,5 @@
-﻿using Punto_de_Venta;
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Punto_de_Venta;
 using Punto_de_Venta.Clases;
 using PUNTOVENTA.CLASES;
 using PUNTOVENTA.ENTIDAD;
@@ -11,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = ClosedXML.Excel;
 
 namespace PUNTOVENTA.MENU.REPORTES
 {
@@ -277,63 +279,165 @@ namespace PUNTOVENTA.MENU.REPORTES
 
         private void button2_Click(object sender, EventArgs e)
         {
-            List<List<string>> ListadeListas = new List<List<string>>();
-           
 
-            List<string> ListaDatos = new List<string>();
+            
+            txtFechai.Text = FechaInicio.Value.ToString("dd/MM/yyyy");
+            txtFechaf.Text = fechafinal.Value.ToString("dd/MM/yyyy");
 
+            dgReportes parametro = new dgReportes();
 
-            string v1 = "";
-            string v2 = "";
-            string v3 = "";
-            string v4 = "";
-            string v5 = "";
-            string v6 = "";
-            string v7 = "";
-
-
-
-
-
-            for (int fila = 0; fila < dataGridView1.Rows.Count - 1; fila++)
+            if (bx_categoria.Text != "" && bx_productos.Text != "")
             {
-                for (int col = 0; col < dataGridView1.Rows[fila].Cells.Count; col++)
-                {
-                    string valor = dataGridView1.Rows[fila].Cells[col].Value.ToString();
-                   
+
+                parametro.FechaInicio = Convert.ToDateTime(txtFechai.Text);
+                parametro.FechaFinal = Convert.ToDateTime(txtFechaf.Text);
+
+                parametro.IdCategoria = Convert.ToInt16(lbl_id_categoria.Text);
+                parametro.IdProducto = Convert.ToInt16(lbl_id_producto.Text);
 
 
-                    ListaDatos.Add(valor);
-                  
-
-                   
-                }
-                ListadeListas.Add(ListaDatos);
-                ListaDatos.Clear();
             }
-           
+
+            else if (bx_categoria.Text != "" && bx_productos.Text == "")
+            {
+
+                parametro.FechaInicio = Convert.ToDateTime(txtFechai.Text);
+                parametro.FechaFinal = Convert.ToDateTime(txtFechaf.Text);
+
+                parametro.IdCategoria = Convert.ToInt16(lbl_id_categoria.Text);
+                parametro.IdProducto = 0;
+
+
+
+            }
+
+            else
+            {
+
+                parametro.FechaInicio = Convert.ToDateTime(txtFechai.Text);
+                parametro.FechaFinal = Convert.ToDateTime(txtFechaf.Text);
+
+                parametro.IdCategoria = 0;
+                parametro.IdProducto = 0;
+
+
+            }
+
+
 
 
             string fechainicio, fechafinal2;
 
           
 
-            fechainicio = FechaInicio.Value.ToString("dd_MM_yyyy");
-            fechafinal2 = fechafinal.Value.ToString("dd_MM_yyyy");
+            fechainicio = FechaInicio.Value.ToString("dd/MM/yyyy");
+            fechafinal2 = fechafinal.Value.ToString("dd/MM/yyyy");
+
+            string fechainicio3 = FechaInicio.Value.ToString("dd_MM_yyyy");
+            string fechafinal3 = fechafinal.Value.ToString("dd_MM_yyyy");
+
+            List<dgReportes> lista = c_reportes.LeerReporte(2, parametro);
+
+
+            if (lista.Count > 0)
+
+            {
+                string ls_archivo_excel = "C:\\C#\\R_Compras" + fechainicio3 + "_" + fechafinal3 + ".xlsx";
+                string fechaentrada;
+
+                //Ruta del fichero Excel
+                int contadorregistros = 7;
+
+                var wb = new Excel.XLWorkbook();
+
+                //create 'worksheet' object
+                var ws = wb.Worksheets.Add("R_Compras" + fechainicio3 + "_" + fechafinal3);
+
+                //read cells
+                var a = ws.Cell("A1").Value;
+                var b = ws.Cell("B1").Value;
 
 
 
-            //foreach (string list in ListadeListas)
-            //{
-            //    MessageBox.Show(list);
+                //write cells
+                ws.Cell("A1").Value = "FILTROS";
+                ws.Cell("G4").Value = "REPORTE COMPRAS ENTRADA";
+                ws.Cell("A2").Value = "FECHA INICIO:";
+                ws.Cell("B2").Value = fechainicio;
+                ws.Cell("A3").Value = "FECHA FINAL:";
+                ws.Cell("B3").Value = fechafinal2;
+
+                ws.Cell("D2").Value = "CATEGORIA:";
+                ws.Cell("D3").Value = "PRODUCTO:";
+
+                ws.Cell("E2").Value = bx_categoria.Text;
+                ws.Cell("E3").Value = bx_productos.Text;
+                ws.Range("A5:G5").Value = "----------------------------------------";
+
+
+                ws.Cell("A6").Value = "No.Entrada";
+                ws.Cell("B6").Value = "No.Producto";
+
+                ws.Cell("C6").Value = "Nombre Producto";
+
+                ws.Cell("D6").Value = "Categoria";
+
+                ws.Cell("E6").Value = "Cantidad";
+
+                ws.Cell("F6").Value = "Fecha Entrada";
+
+                ws.Cell("G6").Value = "Usuario";
+
+                int contadorcolumnas = 1;
+
+                foreach (dgReportes d in lista)
+                {
+
+                    contadorcolumnas = 1;
+                    fechaentrada = d.FechaEntrada.Value.ToString("dd/MM/yyyy");
+                   
+
+
                 
-              
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = d.IdEntrada.ToString();
+                    contadorcolumnas = contadorcolumnas + 1;
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = d.IdProducto.ToString();
+                    contadorcolumnas = contadorcolumnas + 1;
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = d.NombreProducto.ToString();
+                    contadorcolumnas = contadorcolumnas + 1;
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = d.Categoria.ToString();
+                    contadorcolumnas = contadorcolumnas + 1;
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = d.CantidadProducto.ToString();
+                    contadorcolumnas = contadorcolumnas + 1;
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = fechaentrada;
+                    contadorcolumnas = contadorcolumnas + 1;
 
-            //}
+                    ws.Cell(contadorregistros, contadorcolumnas).Value = d.Usuario.ToString();
 
-            string control = "";
-            string ls_archivo_excel = "C:\\C#\\R_Compras"+fechainicio+"_"+fechafinal2 + ".xlsx";
-            control = c_excel.ReporteCompras(ls_archivo_excel,fechainicio,fechafinal2, ListadeListas);
+
+
+                    contadorregistros = contadorregistros + 1;
+
+
+
+
+
+
+                }
+
+                wb.SaveAs(ls_archivo_excel);
+
+            }
+
+            else
+            {
+
+                MessageBox.Show("No se encontró reporte de compras con los filtros seleccionado");
+
+            }
+
+
+
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
